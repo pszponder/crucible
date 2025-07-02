3325986a1e787ab7bda7303e85b0ae1374f877b5
pszponder <14116413+pszponder@users.noreply.github.com>
2025-07-01 21:40:14 -0600

    fix: correct command for setting battery profile in power.sh

--- install/power.sh
+++ install/power.sh
@@ -6,16 +6,19 @@ source $HOME/.local/share/crucible/install/_utils.sh
 
 programs=(
   power-profiles-daemon
+  python-gobject
 )
 
 # Call the install_programs function and pass the tools list
 install_programs "${programs[@]}"
 
+# Ensure python is installed (via mise)
+
 # Setting the performance profile can make a big difference. By default, most systems seem to start in balanced mode,
 # even if they're not running off a battery. So let's make sure that's changed to performance.
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
-  powerprofilesctl set balanced
+  esctl set balanced
 else
   # This computer runs on power outlet
   powerprofilesctl set performance
