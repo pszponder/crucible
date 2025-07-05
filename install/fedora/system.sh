169a71b716b46dea0e57ef28cbc58504f5e1e25f
pszponder <14116413+pszponder@users.noreply.github.com>
2025-07-05 14:16:20 -0600

    fix: adding sudo to terra repo install line

--- install/fedora/system.sh
+++ install/fedora/system.sh
@@ -8,21 +8,27 @@ set -euo pipefail
 sudo dnf upgrade -y
 
 # Enable RPM Fusion repositories for Fedora
+echo "Enabling RPM Fusion repositories for Fedora"
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Install Terra repository for Fedora
-dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+echo "Installing Terra Repository for Fedora"
+sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
 # Install Multimedia codecs
+echo "Installing multimedia codecs"
 sudo dnf install libavcodec-freeworld
 
 # Switch to full ffmpeg
+echo "Switching to full ffmpeg"
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 
 # Install additional codecs
+echo "Installing additional codecs"
 sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
 # Install Hardware codecs w/ AMD (mesa)
+echo "Installing AMD hardware codecs"
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
