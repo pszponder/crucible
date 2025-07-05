626551c07de1ae1d5cf640c8ba1d98c8a43996f3
pszponder <14116413+pszponder@users.noreply.github.com>
2025-07-05 15:12:11 -0600

    fix: remove broken nushell install for fedora

--- install/fedora/cli.sh
+++ install/fedora/cli.sh
@@ -63,11 +63,3 @@ programs_brew=(
 install_programs_dnf "${programs_dnf[@]}"
 install_programs_brew "${programs_brew[@]}"
 
-# Install nushell
-echo "[gemfury-nushell]
-name=Gemfury Nushell Repo
-baseurl=https://yum.fury.io/nushell/
-enabled=1
-gpgcheck=0
-gpgkey=https://yum.fury.io/nushell/gpg.key" | sudo tee /etc/yum.repos.d/fury-nushell.repo
-sudo dnf install -y nushell
